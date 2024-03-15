74929e701af480931b19df97dc49f4fd72de5e9e
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-03-15 18:27:51 +0300

    update Lesson_4:Task02

--- Lesson_4/Task02.py
+++ Lesson_4/Task02.py
@@ -9,3 +9,11 @@ stroka = 'abc bcd abc bcd wwe \n qwe wwe qwe eeq'
 stroka = stroka.split()
 
 print(len(set(stroka)))
+
+
+
+# решение с помощью генераторных выражений
+
+stroka = 'abc bcd abc bcd wwe qwe wwe qwe eeq'
+
+print({i:ord(i) for i in stroka})
