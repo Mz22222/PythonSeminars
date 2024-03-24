248573fd3a1dac36da6cebbc76cba386aa94c34a
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-03-24 15:13:18 +0300

    add homework06:Task02

--- homework06/Task02.py
+++ homework06/Task02.py
@@ -1,43 +1,32 @@
-# Элементы из заданного диапазона
-# Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума).
-# На вход подается список с элементамиlist_1 и границы диапазона в виде чисел min_number, max_number.
+# Арифметическая прогрессия
+# Заполните массив элементами арифметической прогрессии. Её первый элемент a1 , разность d и количество элементов n будет задано автоматически.
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Пример
 # На входе:
-# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-# min_number = 0
-# max_number = 10
+# a1 = 2
+# d = 3
+# n = 4
 # На выходе:
-# 1
 # 2
-# 3
-# 6
-# 7
-# 9
-# 10
+# 5
+# 8
 # 11
-# 13
-# 14
-# 16
-# 19
 
 
-
-list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-min_number = 0
-max_number = 10
+a1 = 2
+d = 3
+n = 4
 
 # Введите ваше решение ниже
 
-def elements_from_diap(list_1, min_number, max_number):
+def arif_progress(a1, d, n):
+    res = []
+    for i in range(n):
+        an = a1 + i * d
+        res.append(an)
+    return res
 
-    result = []
-    for i in range(len(list_1)):
-        if min_number <= list_1[i] <= max_number:
-            result.append(i)
-    return result
-    
 
-result_i = elements_from_diap(list_1, min_number, max_number)
-for index in result_i:
-    print(index)
-
+result = arif_progress(a1, d, n)
+for i in result:
+    print(i)
