921c06f087ed57aa898e0c91cf365f44e9ea7790
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-03-30 00:05:38 +0300

    update Lesson_8:Task01(add a search)

--- Lesson_8/Task01.py
+++ Lesson_8/Task01.py
@@ -1,54 +1,3 @@
-# import os
-# import sys
-
-
-# def add_new_user(name: str, phone: str, filename: str):
-#     """
-#     Добавление нового пользователя.
-#     """
-#     pass
-
-
-# def read_all(filename: str) -> str:
-#     """
-#     Возвращает все содержимое телефонной книги.
-#     """
-#     with open("Number.txt", "r", encoding="utf-8") as file:
-#         return file.read()
-
-
-# def search_user(data: str) -> str:
-#     """
-#     Поиск записи по критерию data.
-#     """
-#     pass
-
-
-# INFO_STRING = """
-# Выберите режим работы:
-# 1 - вывести все данные
-# 2 - добавление нового пользователя
-# 3 - поиск
-# """
-
-# file = "Number.txt"
-
-# if file not in os.listdir():
-#     print("указанное имя файла отсутсвует")
-#     sys.exit()
-
-
-# while True:
-#     mode = int(input(INFO_STRING))
-#     if mode == 1:
-#         print(read_all(file))
-#         pass
-#     elif mode == 2:
-#         pass
-#     elif mode == 3:
-#         pass
-
-
 import os
 import sys
 
@@ -58,12 +7,8 @@ def add_new_user(name: str, phone: str, filename: str):
     Добавление нового пользователя.
     """
     new_line = '\n' if read_all(filename) != "" else ''
-
     with open(filename, "a", encoding="utf-8") as file:
         file.write(f"{new_line}{name} - {phone}")
-
-
-
 
 
 def read_all(filename: str) -> str:
@@ -74,11 +19,20 @@ def read_all(filename: str) -> str:
         return file.read()
 
 
-def search_user(data: str) -> str:
+def search_user(filename: str, data: str) -> str:
     """
     Поиск записи по критерию data.
     """
-    pass
+    with open(filename, "r", encoding="utf-8") as file:
+        list_1 = file.read().split("\n")
+    result = []
+    result = [i for i in list_1 if data in i]
+    # for i in list_1:
+    # if data in i:
+    # result.append(i)
+    if not result:
+        return "По указанному значению совпадений не найдено"
+    return "\n".join(result)
 
 
 INFO_STRING = """
@@ -95,16 +49,14 @@ if file not in os.listdir():
     sys.exit()
 
 
-
 while True:
     mode = int(input(INFO_STRING))
     if mode == 1:
         print(read_all(file))
-        pass
     elif mode == 2:
         name = input("Введите Ваше имя: ")
         phone = input("Введите Ваш телефон: ")
         add_new_user(name, phone, file)
-        pass
     elif mode == 3:
-        pass
+        data = input("Введите значение: ")
+        print(search_user(file, data))
