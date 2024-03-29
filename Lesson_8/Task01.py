5154ae203b63ae5077ac9eee7c5dba253c6f367b
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-03-29 23:44:27 +0300

    update Lesson_8:Task01(add a new user)

--- Lesson_8/Task01.py
+++ Lesson_8/Task01.py
@@ -1,3 +1,54 @@
+# import os
+# import sys
+
+
+# def add_new_user(name: str, phone: str, filename: str):
+#     """
+#     Добавление нового пользователя.
+#     """
+#     pass
+
+
+# def read_all(filename: str) -> str:
+#     """
+#     Возвращает все содержимое телефонной книги.
+#     """
+#     with open("Number.txt", "r", encoding="utf-8") as file:
+#         return file.read()
+
+
+# def search_user(data: str) -> str:
+#     """
+#     Поиск записи по критерию data.
+#     """
+#     pass
+
+
+# INFO_STRING = """
+# Выберите режим работы:
+# 1 - вывести все данные
+# 2 - добавление нового пользователя
+# 3 - поиск
+# """
+
+# file = "Number.txt"
+
+# if file not in os.listdir():
+#     print("указанное имя файла отсутсвует")
+#     sys.exit()
+
+
+# while True:
+#     mode = int(input(INFO_STRING))
+#     if mode == 1:
+#         print(read_all(file))
+#         pass
+#     elif mode == 2:
+#         pass
+#     elif mode == 3:
+#         pass
+
+
 import os
 import sys
 
@@ -6,14 +57,20 @@ def add_new_user(name: str, phone: str, filename: str):
     """
     Добавление нового пользователя.
     """
-    pass
+    new_line = '\n' if read_all(filename) != "" else ''
+
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(f"{new_line}{name} - {phone}")
+
+
+
 
 
 def read_all(filename: str) -> str:
     """
     Возвращает все содержимое телефонной книги.
     """
-    with open("Number.txt", "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         return file.read()
 
 
@@ -38,12 +95,16 @@ if file not in os.listdir():
     sys.exit()
 
 
+
 while True:
     mode = int(input(INFO_STRING))
     if mode == 1:
         print(read_all(file))
         pass
     elif mode == 2:
+        name = input("Введите Ваше имя: ")
+        phone = input("Введите Ваш телефон: ")
+        add_new_user(name, phone, file)
         pass
     elif mode == 3:
         pass
