1eb636d7a0e67888d0a43692cbe6d264030e3672
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-03-31 20:54:38 +0300

    add homework09:Task01

---
+++ homework09/Task01.py
@@ -0,0 +1,14 @@
+# ЗДЕСЬ НЕ РУБИТ!!
+# ЭТО ДЛЯ КОЛАБА, НО СОХРАНЮ ЗДЕСЬ, ЧТОБЫ БЫЛО 
+
+
+# Определить среднюю стоимость дома
+
+# Дан файл california_housing_train.csv. Определить среднюю стоимость дома , где количество людей от 0 до 500 (population) и сохранить ее в переменную avg.
+# Используйте модуль pandas.
+
+import pandas 
+
+df = pandas.read_csv('california_housing_train.csv')
+
+avg = df[(df['population'] >= 0) & (df['population'] <= 500)]['median_house_value'].mean()
